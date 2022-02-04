bd52c0a6a0f72ff9c940fc1615c5c2dd62ea469d
21ciprian <2021ciprianoffice@gmail.com>
2022-02-04 21:52:35 +0000

    Styled EventCard component

--- src/components/EventCard/index.jsx
+++ src/components/EventCard/index.jsx
@@ -4,22 +4,29 @@ import styles from '../../../styles/EventCard.module.css'
 import ButtonComponent from '../ButtonComponent'
 
 function EventCard({event}) {
-	if (!event) {
-		return <h2>No event found...</h2>
-	}
 	return (
 		<div className={styles.card}>
-			<h2>Event: {event.name}</h2>
+			<h2 className={styles.name}>
+				<strong>{event.name}</strong>
+			</h2>
 			<Image
+				className={styles.image}
 				width={200}
 				height={300}
 				src={`${event.images[0].url}`}
 				alt={event.name}
 			/>
-
-			<h2>Venue: {event._embedded.venues[0].name}</h2>
-			<p>Date: {event.dates.start.localDate}</p>
-			<p>Start Time: {event.dates.start.localTime}</p>
+			<div className={styles.details}>
+				<h2>
+					Venue: <strong>{event._embedded.venues[0].name}</strong>
+				</h2>
+				<p>
+					Date: <strong>{event.dates.start.localDate}</strong>
+				</p>
+				<p>
+					Start Time: <strong>{event.dates.start.localTime}</strong>
+				</p>
+			</div>
 			<ButtonComponent text='	Buy Ticket'>
 				<a href={event.url} target='_blank noopener norefferer'></a>
 			</ButtonComponent>
